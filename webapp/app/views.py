71fc638c8df6f5563f2a5f7d621f008b6586181e
Charles Verdad <charles.verdad@gmail.com>
2017-10-21 04:41:31 +0800

    query file types

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,8 +1,25 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from app import app
-
+from app.models import File
+from app import db
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+
+@app.route('/getContent', methods=['GET'])
+def getContent():
+    filetype = request.args.get('filetype')
+    files = db.session.query(File).filter(File.filetype == filetype).all()
+    out = [
+        {
+            "name": file.name,
+            "description": file.description,
+            "path": file.path
+        }
+        for file in files
+    ]
+    return jsonify(out)
--- webapp/config.py
+++ webapp/config.py
@@ -3,3 +3,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+
+supported_extensions = {
+	'doc': {'pdf'},
+	'video': {'mp4'},
+	'audio': {'mp3'}
+}
+
+
+KNOWN_FILETYPES = ['mp4']
